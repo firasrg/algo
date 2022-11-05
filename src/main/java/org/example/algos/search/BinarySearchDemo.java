95d013983268fe94c5e8393075ca4bb907189c6a
Firas REGAYEG <fregayeg@iga-tunisie.com>
2022-11-05 21:00:23 +0100

    Commit 4: binary search example - 2

--- src/main/java/org/example/algos/search/BinarySearchDemo.java
+++ src/main/java/org/example/algos/search/BinarySearchDemo.java
@@ -2,14 +2,43 @@ package org.example.algos.search;
 
 import java.util.Arrays;
 
+/**
+ * Firas1220
+ */
 public class BinarySearchDemo {
 
+    private static int binarySearchIterative(int[] arr, int key, int start, int end) {
 
-    private static int binarySearch(int[] arr, int key, int start, int end) {
+        if(arr.length == 0)
+            return -1;
 
-        System.out.println("start: " + start);
-        System.out.println("end: " + end);
+        int midIndex = (start + end) / 2;
 
+        while (start < end || start == end) {
+
+            if (arr[midIndex] == key)
+                return midIndex;
+
+            if (arr[midIndex] > key)
+                end = midIndex - 1;
+
+            if (arr[midIndex] < key)
+                start = midIndex + 1;
+
+            midIndex =  (start + end) / 2;
+
+        }
+
+        return -1;
+    }
+
+    private static int binarySearchRecurive(int[] arr, int key, int start, int end) {
+
+//        System.out.println("start: " + start);
+//        System.out.println("end: " + end);
+
+        if(arr.length == 0)
+            return -1;
 
         int middleIndex = (start + end) / 2;
         int current = arr[middleIndex];
@@ -21,20 +50,21 @@ public class BinarySearchDemo {
             return -1;
 
         if (key < current)
-            return binarySearch(arr, key, start, middleIndex - 1);
+            return binarySearchRecurive(arr, key, start, middleIndex - 1);
 
         else if (key > current)
-            return binarySearch(arr, key, middleIndex + 1, end);
+            return binarySearchRecurive(arr, key, middleIndex + 1, end);
 
         return -1;
     }
 
     public static void main(String[] args) {
 
-        int[] arr = new int[]{-40, -2, 0 , 3, 45 , 99 , 100};
-        int key = 0;
+        int[] arr = new int[]{2, 5};
+        int key = 2;
 
-        System.out.println(binarySearch(arr, key, 0 , arr.length -1));
+        System.out.println(binarySearchRecurive(arr, key, 0 , arr.length -1));
+        System.out.println("result is: " + binarySearchIterative(arr, key, 0, arr.length - 1));
 
     }
 }
